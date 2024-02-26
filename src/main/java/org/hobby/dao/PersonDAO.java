f66e1ed922bcaeff1e5a4a0c237008ea0f8bc74e
altinkaya <124771353+altinkaya@users.noreply.github.com>
2024-02-26 22:25:40 +0100

    ny mangler hiperconfig

--- src/main/java/org/hobby/dao/PersonDAO.java
+++ src/main/java/org/hobby/dao/PersonDAO.java
@@ -1,4 +1,28 @@
 package org.hobby.dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.hobby.model.Person;
+
+import java.util.List;
+
 public class PersonDAO {
+
+    private final EntityManager entityManager;
+    private static final PersonDAO instance = new PersonDAO();
+
+    private PersonDAO() {
+        this.entityManager = HibernateConfig.getEntityManagerFactory();
+    }
+
+    public static PersonDAO getInstance() {
+        return instance;
+    }
+
+    public List<Person> getPersonsByCity(String city) {
+        String jpql = "SELECT p FROM Person p WHERE p.address.city = :city";
+        TypedQuery<Person> query = entityManager.createQuery(jpql, Person.class);
+        query.setParameter("city", city);
+        return query.getResultList();
+    }
 }
