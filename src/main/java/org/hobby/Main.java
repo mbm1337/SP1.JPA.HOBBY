08d4ac51cbd779af287211f88b44932efa56dce3
sumaiak <cph-se160@cphbusiness.dk>
2024-02-28 11:05:13 +0100

    user story 3

--- src/main/java/org/hobby/Main.java
+++ src/main/java/org/hobby/Main.java
@@ -2,25 +2,14 @@ package org.hobby;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jdk.swing.interop.SwingInterOpUtils;
 import org.hobby.config.HibernateConfig;
-import org.hobby.dao.HobbyDAO;
 import org.hobby.dao.PersonDAO;
-import org.hobby.model.Hobby;
 import org.hobby.model.Person;
 import org.hobby.model.ZipCode;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+
     }
-
-
 }
-
-
-
-
-
